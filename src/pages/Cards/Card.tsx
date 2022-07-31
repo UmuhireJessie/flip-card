41838dee68d39913c88ea4d8a0ce0dd81b33c59e
UmuhireJessie <j.umuhire@alustudent.com>
2022-07-31 21:29:40 +0200

    make cards flip

--- src/pages/Cards/Card.tsx
+++ src/pages/Cards/Card.tsx
@@ -1,51 +1,76 @@
+import React from "react";
+import ReactCardFlip from "react-card-flip";
 import UserNavbar from "../../Navbar/Navbar";
 import "./Card.css";
-import { useState } from "react";
-import ReactCardFlip from "react-card-flip";
 
-function Card() {
-  const [isFlipped, setIsFlipped] = useState(false);
-  let id:number;
-  const handleClick = () => {
-    setIsFlipped(!isFlipped);
+
+export default class index extends React.Component<{}, { flipped }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      flipped: new Set()
+    };
   }
 
-  return (
-    <div>
-      <UserNavbar />
+  handleClick(id) {
+    return (e) => {
+      e.preventDefault();
+      let flipped = new Set(this.state.flipped);
+      if (flipped.has(id)) {
+        flipped.delete(id);
+      } else {
+        flipped.add(id);
+      }
+      this.setState({ flipped });
+    };
+  }
 
-      <div className="allCards">
-        <div className="card-container">
-          <ReactCardFlip isFlipped={isFlipped} flipDirection='horizontal' >
-            <div className="question">
-              <h1> Question</h1>
-              <p>What is ICT in full?</p>
-              <button onClick={handleClick}>Check Answer</button>
+  render() {
+    return (
+      <>
+        <UserNavbar />
+        <div className="card-bg">
+          <div className="allCards">
+            <div className="card">
+              <ReactCardFlip
+                isFlipped={this.state.flipped.has(1)}
+                flipDirection="horizontal"
+              >
+                <div className="question">
+                  <h1> Question</h1>
+                  <p>What is ICT in full?</p>
+                  <button className="btn" onClick={this.handleClick(1)}>Check Answer</button>
+                </div>
+                <div className="answer">
+                  <h1> Answer</h1>
+                  <p>Information Communication Technology </p>
+                  <button className="btn" onClick={this.handleClick(1)}>Check Question</button>
+                </div>
+              </ReactCardFlip>
             </div>
-            <div className="answer">
-              <h1> Answer</h1>
-              <p>Information Communication Technology </p>
-              <button onClick={handleClick}>Check Question</button>
-            </div>
-          </ReactCardFlip>
-        </div>
 
-        <div className="card-container">
-          <ReactCardFlip isFlipped={isFlipped} flipDirection='horizontal' >
-            <div className="question">
-              <h1> Question</h1>
-              <p>What is IT in full?</p>
-              <button onClick={handleClick}>Check Answer</button>
+            <div className="card">
+              <ReactCardFlip
+                isFlipped={this.state.flipped.has(2)}
+                flipDirection="horizontal"
+              >
+                <div className="question">
+                  <h1> Question</h1>
+                  <p>What is IT in full?</p>
+                  <button className="btn" onClick={this.handleClick(2)}>Check Answer</button>
+                </div>
+                <div className="answer">
+                  <h1> Answer</h1>
+                  <p>Information Technology </p>
+                  <button className="btn" onClick={this.handleClick(2)}>Check Question</button>
+                </div>
+              </ReactCardFlip>
             </div>
-            <div className="answer">
-              <h1> Answer</h1>
-              <p>Information Technology </p>
-              <button onClick={handleClick}>Check Question</button>
-            </div>
-          </ReactCardFlip>
+          </div>
         </div>
-      </div>
-    </div>
-  );
+      </>
+
+
+    );
+  }
 }
-export default Card;
